33917224c6c639a0ea23da705ef950d885b941bb
Igor Ghirardelli <igorghirardelli@gmail.com>
2022-07-28 15:31:14 -0300

    nome/idade teste

---
+++ idadeteste/src/idadeteste/Idadeteste.java
@@ -0,0 +1,19 @@
+
+package idadeteste;
+
+import java.util.Scanner;
+
+
+public class Idadeteste {
+
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+         System.out.println("digite seu nome e sua idade! ");
+        String nome =  teclado.nextLine();
+        float nota =  teclado.nextFloat(); 
+        System.out.println("seu nome é:" +nome);
+        System.out.println("e sua idade é:"+nota);
+    }
+    
+}
