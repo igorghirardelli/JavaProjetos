1c0d2ee33440a40e195c2650daf60091af305408
Igor Ghirardelli <igorghirardelli@gmail.com>
2022-08-29 18:16:36 -0300

    Exercicio

--- ExerciciosAula17/src/exerciciosaula17/Exercicio30.java
+++ ExerciciosAula17/src/exerciciosaula17/Exercicio30.java
@@ -4,12 +4,44 @@
  */
 package exerciciosaula17;
 
+import java.util.Scanner;
+
 /**
  *
  * @author igora
  */
 public class Exercicio30 {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Entre com o numero para gerar a tabuada ");
+        int num = teclado.nextInt();
+        
+        boolean invalido = true;
+        int numfinal;
+        int numinicio;
+        do {
+            System.out.println("Entre com o inicio da tabuada");
+        numinicio = teclado.nextInt();
+        
+        System.out.println("Entre com o final da tabuada ");
+         numfinal = teclado.nextInt();
+        
+        if(numfinal > numinicio){
+            invalido = false;
+        }
+        }while(invalido);
+        
+        
+        
+        System.out.println("Tabuada de :" + num + ":");
+        System.out.println("Campo por :" + numinicio);
+        System.out.println("Termina por :" + numfinal);
+        System.out.println();
+        
+        for(int i = numinicio; i <=numfinal; i++){
+            System.out.println(num+ "x" + i + "=" +(num*i));
+        }
         
     }
 }
